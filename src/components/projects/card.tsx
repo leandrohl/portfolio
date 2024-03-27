2291032cc867b46c528097ba4447e7ea65937f2d
unknown <leandrohenrique.lima55@gmail.com>
2024-03-27 19:07:45 -0300

    fix: ajuste responsividade card de projetos

--- src/components/projects/card.tsx
+++ src/components/projects/card.tsx
@@ -9,8 +9,8 @@ interface CardPropos {
 export default function Card({ title, description, image, redirect }: CardPropos){
     return(
         <a href={redirect} target="blank" className="flex flex-col md:flex-row items-center my-12">
-            <img className="md:w-2/4" src={image} alt={title}></img>
-            <div className="mt-4 md:mt-0 md:ml-8">
+            <img  className="md:w-2/4" src={image} alt={title}></img>
+            <div className="mt-4 md:mt-0 md:ml-8 md:w-2/4">
                 <h2 className="text-2xl text-white font-bold">{title}</h2>
                 <p className="text-gray mt-6 ">{description}</p>
                 <div className="flex w-full justify-center md:justify-start">
