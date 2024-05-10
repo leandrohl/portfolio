63103872998b01bb48c53e63e66e86a7b5242614
Leandro Silva <leandro.silva.ext@vr.com.br>
2024-05-10 11:15:03 -0300

    feat: adicionando projetos

--- src/components/projects/card.tsx
+++ src/components/projects/card.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import Redirect from "../../assets/images/redirect.png"
 
 interface CardPropos {
     title: string;
@@ -7,18 +9,29 @@ interface CardPropos {
 }
 
 export default function Card({ title, description, image, redirect }: CardPropos){
+    const [open, setOpen] = useState(false);
+
+
     return(
-        <a href={redirect} target="blank" className="flex flex-col md:flex-row items-center my-12">
-            <img className="md:w-2/4" src={image} alt={title}></img>
-            <div className="mt-4 md:mt-0 md:ml-8">
-                <h2 className="text-2xl text-white font-bold">{title}</h2>
-                <p className="text-gray mt-6 ">{description}</p>
-                <div className="flex w-full justify-center md:justify-start">
-                    <button className="mt-10 bg-primary py-4 px-10 rounded-md font-semibold text-white ">
-                        Link do projeto
-                    </button>
+        <div 
+            className="flex flex-col my-12 relative" 
+            onMouseEnter={() => setOpen(true)} 
+            onMouseLeave={() => setOpen(false)} 
+        >
+            { open 
+            ? (
+                <div className="absolute top-0 bottom-0	w-full flex flex-col justify-center items-center mt-4">
+                    <h2 className="text-2xl text-white font-bold">{title}</h2>
+                    <p className="text-gray w-3/4 mt-6 text-center">{description}</p>
+                    <div className="flex  justify-center ">
+                        <a href={redirect} target="blank" className="mt-10 bg-primary p-4 rounded-full font-semibold text-white ">
+                        <img className="h-8" src={Redirect} alt={title}></img> 
+                        </a>
+                    </div>
                 </div>
-            </div>
-        </a>
+            ) 
+            : <img className="h-96 " src={image} alt={title}></img> 
+            }
+        </div>
     )
 }
